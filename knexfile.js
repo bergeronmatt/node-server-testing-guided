8b19c3f66dc674b60e9bf37926643099168f1865
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-23 12:40:44 -0400

    knex change

--- knexfile.js
+++ knexfile.js
@@ -24,5 +24,16 @@ module.exports = {
     seeds: {
       directory: "./data/seeds",
     },
+    //Heroku will look for a prouction config
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL, //provided by Heroku
+      migrations: {
+        directory: "./data/migrations",
+      },
+      seeds: {
+        directory: "./data/seeds",
+      },
+    }
   },
 };
